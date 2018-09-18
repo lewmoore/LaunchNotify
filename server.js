7185008cab8538b2d55742b6aef07f6df1a808d8
lewmoore <lewis.moore2707@gmail.com>
2018-09-18 10:02:51 +0100

    Pass response object to ejs view and render

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
 app.set("view engine", "ejs")
 app.set("views", "views")
 
 app.get('/', function(req, res){
-    res.render('index')
+    fetch('https://api.spacexdata.com/v2/launches/next').then(response => response.json()).then(json => res.render('index', { json: json }))
 })
 
 let port = process.env.PORT || 8080;
