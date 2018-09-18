bd8059bf0bad270f4611e9ac094bb1795fb4721c
lewmoore <lewis.moore2707@gmail.com>
2018-09-18 09:31:07 +0100

    Add exprfess config with Localhost 8080

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-fetch('https://api.spacexdata.com/v2/launches/next').then(function(res) {
-    return res
-})
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+let port = process.env.PORT || 8080;
+app.listen(port)
+console.log("You're on localhost " + port);
+
+module.exports = app
