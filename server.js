e90a2d2807ba6f915b5288f492a766b4d8c34ec4
lewmoore <lewis.moore2707@gmail.com>
2018-09-18 09:33:39 +0100

    Add ejs views with get index route

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+app.set("view engine", "ejs")
+app.set("views", "views")
 
+app.get('/', function(req, res){
+    res.render('index')
+})
 
 let port = process.env.PORT || 8080;
 app.listen(port)
